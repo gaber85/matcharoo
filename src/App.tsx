9083b62d0f35b833a467dde6ea41a238b81bdeac
Gabe <gabriel.riera@sharesight.co.nz>
2022-10-03 22:26:00 +1300

    chore: test action to pass test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
 import type { FC } from 'react';
 
-const test = 'string';
-
-export const App: FC = () => <div className="test">'Hello World!'{test}</div>;
+export const App: FC = () => <div className="test">'Hello World!'</div>;
